534a8fa9ff9d19725698fb5e9bdab7003de0af07
Yogesh Kumbhar <111835554+Yogesh188@users.noreply.github.com>
2022-12-25 12:24:52 +0000

    ex05

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// ex01
-// console.log("Yogesh Kumbhar")
- 
-
-//ex02 to ex04
-// var readlineSync = require('readline-sync');
-// var username =  readlineSync.question("May I know your name? ");
-// console.log('Welcome,'+username+'!');
-
-
 //ex05
 var readlineSync = require('readline-sync');
 var value =readlineSync.question("Guess my age? ");
-if(value<25,
-  )
